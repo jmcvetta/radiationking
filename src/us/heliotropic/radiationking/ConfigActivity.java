e353dba2ad2ab0b91f77d26924310575280598b2
Jason McVetta <jason.mcvetta@gmail.com>
2013-08-20 15:19:57 -0700

    wakeupChoices()

--- src/us/heliotropic/radiationking/ConfigActivity.java
+++ src/us/heliotropic/radiationking/ConfigActivity.java
@@ -1,7 +1,9 @@
 package us.heliotropic.radiationking;
 
-import android.os.Bundle;
+import java.util.TreeMap;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class ConfigActivity extends Activity {
@@ -10,6 +12,8 @@ public class ConfigActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_config);
+		
+		
 	}
 
 	@Override
@@ -18,5 +22,22 @@ public class ConfigActivity extends Activity {
 		getMenuInflater().inflate(R.menu.config, menu);
 		return true;
 	}
+	
+	public static TreeMap<Integer, String> wakeupChoices() {    
+		Integer intervals[] = {10, 15, 30, 60};
+	    TreeMap<Integer, String> map = new TreeMap<Integer,String>();
+
+	    map.put(0, "Never");
+	    
+	    for (Integer i : intervals) {
+	    	String s = "Every ";
+	    	s += i.toString();
+	    	s += " minutes";
+	    	map.put(i, s);
+	    }
+
+	    return map;
+	}
+
 
 }
